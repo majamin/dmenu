0a70b1f28309f8c3e80e3d9ea138f0434eb6c098
Marian Minar <majamin@gmail.com>
2022-11-17 19:24:45 -0800

    basic styles and compile defaults

--- config.def.h
+++ config.def.h
@@ -4,17 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	                /*   fg         bg   */
+	[SchemeNorm] = { "#333333", "#6baadd" },
+	[SchemeSel]  = { "#eeeeee", "#28589c" },
+	[SchemeOut]  = { "#eeeeee", "#3171af" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
